756cac1a53a555692a294d0303e3d1374540e97c
Gheorghe Stegaru <gh.st@icloud.com>
2026-05-07 20:40:35 -0400

    feat(database): persist promotion across launches via shared_preferences

--- features/database/lib/database.dart
+++ features/database/lib/database.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @immutable
 class Promotion {
@@ -13,9 +14,24 @@ class Database {
 
   static final Database instance = Database._();
 
+  static const _cactusKey = 'promotion.cactus';
+  static const _priceKey = 'promotion.price';
+
   final ValueNotifier<Promotion?> promotion = ValueNotifier<Promotion?>(null);
 
-  void savePromotion(Promotion value) {
+  Future<void> init() async {
+    final prefs = await SharedPreferences.getInstance();
+    final cactus = prefs.getString(_cactusKey);
+    final price = prefs.getDouble(_priceKey);
+    if (cactus != null && price != null) {
+      promotion.value = Promotion(cactus: cactus, price: price);
+    }
+  }
+
+  Future<void> savePromotion(Promotion value) async {
     promotion.value = value;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_cactusKey, value.cactus);
+    await prefs.setDouble(_priceKey, value.price);
   }
 }
